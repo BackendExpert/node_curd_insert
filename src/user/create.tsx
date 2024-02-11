cdc48a54f15f23535022938a5505a7e2fb0fbb0f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 05:16:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- src/user/create.tsx
+++ src/user/create.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+
 const create = () => {
+    const [username, SetUsername] = useState("")
+
   return (
     <div className="container mx-auto px-10">
-        
+        <form>
+            <h1>User Add</h1>
+            <div className="mb-4 mt-4">
+                <label htmlFor="">Username</label>
+                <input type="text" name="" id="" className="w-full h-12 border border-gray-500 rounded" />
+            </div>
+            <div className="mb-4 mt-4">
+                <label htmlFor="">Email</label>
+                <input type="email" name="" id="" className="w-full h-12 border border-gray-500 rounded" />
+            </div>
+            <div className="mb-4 mt-4">
+                <label htmlFor="">Mobile</label>
+                <input type="text" name="" id="" className="w-full h-12 border border-gray-500 rounded" />
+            </div>
+        </form>
     </div>
   )
 }
