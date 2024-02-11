15d4c99b90b075aebcd5641c14ec38c9e60eab4a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 05:12:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserCreate from "./user/create";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="User/create" element={<UserCreate />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
